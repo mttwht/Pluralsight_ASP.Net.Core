e3fc748412e4af6a9f4bae1d255269b826fc33dd
Matthew White <Matthew.White02@gmail.com>
2018-10-31 14:59:28 +0000

    Register the MVC service and add a HomeController with Index action

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -18,6 +18,7 @@ namespace OdeToFood
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IGreeter, Greeter>();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,8 +30,10 @@ namespace OdeToFood
             }
 
             //app.UseDefaultFiles();
-            //app.UseStaticFiles();
-            app.UseFileServer(); // shorthand for two lines above
+            app.UseStaticFiles();
+            //app.UseFileServer(); // shorthand for two lines above
+
+            app.UseMvcWithDefaultRoute();
 
             app.Run(async (context) =>
             {
