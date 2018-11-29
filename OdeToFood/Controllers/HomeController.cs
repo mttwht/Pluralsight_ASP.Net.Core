44062e19d64eee8a13c3d49f519f3afa491b29da
Matthew White <Matthew.White02@gmail.com>
2018-11-29 18:15:09 +0000

    Create and register IRestaurantData service Implement InMemoryRestaurantData and use it to retrieve a model

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
         public IActionResult Index()
         {
-            //return Content("Home => Index");
-
-            var model = new Restaurant { Id = 1, Name = "Token" };
+            var model = _restaurantData.GetAll();
 
             return View(model);
         }
---
+++ OdeToFood/Services/InMemoryRestaurantData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> _restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "Token" },
+                new Restaurant { Id = 2, Name = "Home" },
+                new Restaurant { Id=3, Name="Matt's kitchen" },
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll() => _restaurants.OrderBy(r => r.Name);
+    }
+}
