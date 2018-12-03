c8b59a8ded4046ab4c6045ab302d95c041e81411
Matthew White <Matthew.White02@gmail.com>
2018-12-03 13:44:41 +0000

    Create a ViewModel for Home/Index

--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using OdeToFood.Models;
 using OdeToFood.Services;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
         private IRestaurantData _restaurantData;
+        private IGreeter _greeter;
 
-        public HomeController(IRestaurantData restaurantData)
+        public HomeController(IRestaurantData restaurantData,
+                              IGreeter greeter)
         {
             _restaurantData = restaurantData;
+            _greeter = greeter;
         }
 
         public IActionResult Index()
         {
-            var model = _restaurantData.GetAll();
+            var model = new HomeIndexViewModel {
+                Restaurants = _restaurantData.GetAll(),
+                MessageOfTheDay = _greeter.GetMessageOfTheDay(),
+            };
 
             return View(model);
         }
