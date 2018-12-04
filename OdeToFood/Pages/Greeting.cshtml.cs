6618de95cee0a6143de05818b53d627f5815c453
Matthew White <Matthew.White02@gmail.com>
2018-12-04 15:15:10 +0000

    Use Rozor Pages to render a page

---
+++ OdeToFood/Pages/Greeting.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using OdeToFood.Services;
+
+namespace OdeToFood.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private IGreeter _greeter;
+
+        public string MessageOfTheDay { get; private set; }
+
+        public GreetingModel(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public void OnGet(string name)
+        {
+            MessageOfTheDay = $"{_greeter.GetMessageOfTheDay()}; {name}";
+        }
+    }
+}
