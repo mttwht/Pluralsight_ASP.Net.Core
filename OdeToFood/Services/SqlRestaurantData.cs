14c58f0572e26d21b1d5def429ced1d71d55a94c
Matthew White <Matthew.White02@gmail.com>
2018-12-04 14:06:52 +0000

    Create a DbContext for project

---
+++ OdeToFood/Services/SqlRestaurantData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Data;
+using OdeToFood.Models;
+
+namespace OdeToFood.Services
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private OdeToFoodDbContext _context;
+
+        public SqlRestaurantData(OdeToFoodDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public Restaurant Add(Restaurant restaurant)
+        {
+            _context.Restaurants.Add(restaurant);
+            _context.SaveChanges();
+            return restaurant;
+        }
+
+        public Restaurant Get(int id) => _context.Restaurants.FirstOrDefault(r => r.Id == id);
+
+        public IEnumerable<Restaurant> GetAll() => _context.Restaurants.OrderBy(r => r.Name);
+    }
+}
