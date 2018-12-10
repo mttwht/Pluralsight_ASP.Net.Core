289e9377958a91409def6a22665c9e5476987a5c
Matthew White <Matthew.White02@gmail.com>
2018-12-10 14:23:20 +0000

    Register authentication services

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Rewrite;
@@ -25,6 +27,17 @@ namespace OdeToFood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication(options => {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddOpenIdConnect(options => {
+                options.ResponseType = "code";
+                options.Scope.Add("openid email");
+                _config.Bind("OpenID", options);
+            })
+            .AddCookie();
+
             services.AddSingleton<IGreeter, Greeter>();
 
             // Allow restaurant list to exist between requests
@@ -48,6 +61,8 @@ namespace OdeToFood
             app.UseRewriter(new RewriteOptions().AddRedirectToHttpsPermanent());
 
             app.UseStaticFiles();
+
+            app.UseAuthentication();
 
             app.UseMvc(ConfigureRoutes);
 
